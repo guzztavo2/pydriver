58b83f7e8e650abdce4c8cee4e7e331485e11bfb
Gustavo Bonifácio <gustavo_bonifacio2020@outlook.com>
2025-09-18 21:19:02 +0000

    update(docker-compose, readme, startvnc)

--- .devcontainer/start-vnc.sh
+++ .devcontainer/start-vnc.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 export DISPLAY=:99
 
-# Mata restos antigos
 killall -q Xvnc fluxbox || true
 rm -f /tmp/.X99-lock
 rm -rf /tmp/.X11-unix/X99
 
-# Inicia Xvnc (X11 + VNC em um só processo)
 Xvnc :99 -geometry 1280x800 -depth 24 -rfbport ${VNC_PORT:-5900} -SecurityTypes None &
-sleep 1
-
-# Inicia fluxbox
+sleep 2
 fluxbox &
-
-# Mantém o container vivo
+sleep 2
+chromium --no-sandbox --disable-dev-shm-usage --user-data-dir=/tmp/chromium-vnc &
 tail -f /dev/null
